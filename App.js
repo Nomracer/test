f9e6958b16e656358dfb3167a49f9d05e38c1a77
Nomracer <gurkansimsek2153@gmail.com>
2022-06-04 16:54:31 +0300

    master

---
+++ App.js
@@ -0,0 +1,50 @@
+import { StyleSheet,
+   Text,
+   View,
+   SafeAreaView,
+   ScrollView,
+  TouchableHighlight,
+TouchableOpacity } from 'react-native';
+import { useState,useEffect } from 'react';
+
+
+export default function App() {
+  const [isim,setisim]=useState()
+  const [numara,setnumara]=useState(0)
+  useEffect(()=>{
+    setisim("Deniz")
+  },[])
+  return (
+    <SafeAreaView style={styles.mainview}>
+      <ScrollView>
+        
+        <TouchableOpacity activeOpacity={.7} onPress={tikla} style={styles.btn}>
+          <Text >Bana {numara} Kere Tıkladın</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+  );
+  function tikla(){
+    setnumara(numara+1)
+  }
+}
+
+const styles =StyleSheet.create({
+  mainview:{
+    flex:1,
+    flexDirection:'row',
+    flexWrap:'wrap',
+    backgroundColor:'cyan',
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  textview:{
+    color:'black'
+  },
+  btn:{
+    backgroundColor:'white',
+    height:50,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
