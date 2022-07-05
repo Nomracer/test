65c941d5816b827308e3258817b14aa4165f4d5b
FlyingTurkman <dytdenizarrli@hotmail.com>
2022-07-05 15:02:55 +0300

    master

--- Kitapekle.js
+++ Kitapekle.js
@@ -3,14 +3,30 @@ import { Text
 ,StyleSheet
 ,SafeAreaView
 ,View
+,TouchableHighlight
 } from "react-native";  
 
 export default function App(){
     return(
-        <SafeAreaView style={styles.main}></SafeAreaView>
+        <SafeAreaView style={styles.main}>
+            <TouchableHighlight onPress={test}>
+                <Text>Dokun</Text>
+            </TouchableHighlight>
+        </SafeAreaView>
     
     )
-
+    async function test(){
+        fetch('../api/test',{
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                email:'deasdas@gmail.com'
+            })
+        })
+    }
 }
 const styles = StyleSheet.create({
     main:{
